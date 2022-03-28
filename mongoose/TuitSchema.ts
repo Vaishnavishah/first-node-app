17dc377c293d8735a58088c3efbea63ba24fc7de
Vaishnavishah <shahvaishnavi2411@gmail.com>
2022-03-28 12:19:27 -0400

    updated tuit schema to take stats

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -7,5 +7,11 @@ const TuitSchema = new mongoose.Schema({
     tuit: {type: String, required: true},
     postedOn: {type: Date, default: Date.now},
     postedBy: {type: String, required: true},
+    stats: {
+        replies: {type: Number, default: 0},
+        retuits: {type: Number, default: 0},
+        likes: {type: Number, default: 0}
+    }
+
 }, {collection: 'tuits'});
 export default TuitSchema;
